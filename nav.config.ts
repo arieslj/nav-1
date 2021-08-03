e45e4635640117ece1152e410c4bbf97d896f776
arieslj <46982655+arieslj@users.noreply.github.com>
2021-08-03 13:14:55 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -2,7 +2,7 @@ import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/arieslj/nav',
 
   // 部署分支
   branch: 'main',
@@ -26,7 +26,7 @@ const c: IConfig = {
   keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
 
   // 默认主题: Light | Sim | Side | App | Shortcut
-  theme: 'Light',
+  theme: 'Side',
 
   // 网站底部内容, 版权信息、备案号, 可以是 HTML
   footerContent: `
